4d1eb4d53f0659c535090b0212b37c62a4f8092a
William <69988679+Killaship@users.noreply.github.com>
2023-03-02 00:48:54 -0500

    Sync with offline changes

--- main.py
+++ main.py
@@ -4,25 +4,35 @@ import os
 import requests
 import time
 import signal
-data=open('httplist.py','r+').read() 
+import random
+import time
+membercount=0
+totalmessages=0 # total number of messages since bot turned on
+data=open('httplist.py','r+').read()
 exec(data)
+leaderboard = []
+xp = []
 
 class TimeoutException(Exception):   # Custom exception class
     pass
 
-def timeout_handler(signum, frame):   # Custom signal handler
+def timeout_handler(signum, frame):   # Custom signal handler (this is where OSdev IDT knowledge is relatable :p)
     raise TimeoutException
 
-# Change the behavior of SIGALRM
+# Change the behavior of SIGALRM to call the timeout handler
 signal.signal(signal.SIGALRM, timeout_handler)
+
 
 
 key=os.getenv('key')
 wkey=os.getenv('wkey')
 
-client = discord.Client()#declaring what the client is.
+intents = discord.Intents.default()
+intents.members = True
 
-client = commands.Bot(command_prefix = '$')#Makes the bot prefix.
+client = discord.Client() #declaring what the client is.
+
+client = commands.Bot(command_prefix = '$', intents=intents)#Makes the bot prefix.
 client.remove_command('help')#Removes the auto help command as it can be buggy.
 
     # https://discord.com/api/oauth2/authorize?client_id=1079242361491693658&permissions=8&scope=applications.commands%20bot
@@ -36,8 +46,35 @@ async def explain(ctx):
     embed.add_field(name="Where can I get more info?", value="Check out this link: https://github.com/hsanger/survivreloaded")
     await ctx.send(embed=embed)#sends the embed.
 
+@client.event
+async def on_ready():
+    global leaderboard
+    global xp
+    print("========")
+    print("current UNIX time is {time}.".format(time=int(time.time())))
+    print("========")
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('========')
+    print("reloading XP and boards")
+    with open("board.txt") as file:
+        leaderboard = file.read().splitlines()
+        leaderboard = [int(i) for i in leaderboard]
+        file.close()
+    with open("xp.txt") as file:
+        xp = file.read().splitlines()
+        xp = [int(i) for i in xp]
+        file.close()
+    print("done")
+    print("========")
+    print(leaderboard)
+    print(xp)
 
-  
+
+
+
+
 @client.command()
 @commands.is_owner()
 async def shell(ctx,cmd):
@@ -57,11 +94,97 @@ async def links(ctx):
     embed.add_field(name="Subreddit", value="https://reddit.com/r/survivreloaded")
     embed.add_field(name="Github", value="https://github.com/hsanger/survivreloaded")
     embed.add_field(name="GitLab (archived)", value="https://gitlab.com/hasanger/survivreloaded")
+    embed.add_field(name="Bot GitHub", value="https://github.com/Killaship/survivbot")
     await ctx.send(embed=embed)
 
 
 
+@client.command()
+@commands.is_owner()
+async def initleaderboard(ctx):
 
+    global leaderboard
+    global xp
+
+    await ctx.send("Initializing leaderboard, this may take a while.")
+    time.sleep(0.5)
+    await ctx.send("Counting Members")
+    global membercount
+    members = ctx.message.guild.members
+
+    for member in members:
+        await ctx.send(member.id)
+        leaderboard.append(member.id)
+        xp.append(0)
+        membercount += 1
+        time.sleep(.1)
+    await ctx.send("Member counting finished")
+
+    file = open("board.txt", 'w+') 
+    file.truncate(0) # overwrite file
+    for i in range(len(leaderboard)):
+        #file.write(str(leaderboard[i]) + " " + str(xp[i]) + "\n")
+        file.write(str(leaderboard[i]) + "\n")
+    file.close()
+    await ctx.send("Leaderboard exported to board.txt")
+
+    file = open("xp.txt", 'w+') 
+    file.truncate(0) # overwrite file
+    for i in range(len(xp)):
+         file.write(str(xp[i]) + "\n")
+    file.close()
+    await ctx.send("XP counts exported to xp.txt")
+
+async def syncboards():
+    file = open("board.txt", 'w+') 
+    file.truncate(0) # overwrite file
+    for i in range(len(leaderboard)):
+        #file.write(str(leaderboard[i]) + " " + str(xp[i]) + "\n")
+        file.write(str(leaderboard[i]) + "\n")
+    file.close()
+    print("Leaderboard exported to board.txt")
+    file = open("xp.txt", 'w+') 
+    file.truncate(0) # overwrite file
+    for i in range(len(xp)):
+         file.write(str(xp[i]) + "\n")
+    file.close()
+    print("XP counts exported to xp.txt")
+
+
+@client.event
+async def on_message(message):
+    id = message.author.id
+    global totalmessages
+    incXP = random.randrange(5, 10)
+    totalmessages += 1
+
+    if id in leaderboard: # If the ID is on the leaderboard...
+
+        index = leaderboard.index(id) # Find where the ID is on the leaderboard
+
+        xp[index] += incXP # Increment corresponding xp by between 5 and 10 points
+
+        print("User ID {userid} gained {xpamount}".format(userid=id,xpamount=incXP))
+
+    else: # If user ID isn't in message
+
+        print("User ID {userid} is not on leaderboard. Run $initleaderboard again?".format(userid=id))
+
+        #print("Note: $initleaderboard resets leaderboard, spams, takes a long time.") 
+    await syncboards()
+    
+    await client.process_commands(message) # Lets bot process other commands after event is done
+    
+
+
+
+@client.command()
+async def getxp(ctx):
+    if ctx.author.id in leaderboard: # If the ID is on the leaderboard...
+        index = leaderboard.index(ctx.author.id) # Find where the ID is on the leaderboard
+        await ctx.send("<@{id}> has {xp} XP!".format(id=ctx.author.id,xp=xp[index]))   
+    else:
+        await ctx.send("Error! <@{userid}> is not on the leaderboard. :/".format(userid=ctx.author.id))
 
 @client.command()
 async def help(ctx):
@@ -77,6 +200,8 @@ async def help(ctx):
     embed.add_field(name="$links", value="Lists various links related to the project.")
 
     embed.add_field(name="$serverstatus", value="Checks whether the game server (or at least website) is up.")
+
+    embed.add_field(name="$getxp", value="This command shows the amount of XP the sender has.")
 
     embed.add_field(name="$checkurl", value="Checks the connectivity of any URL.")
 
@@ -119,13 +244,6 @@ def urlcheck(url):
     except TimeoutException:
         return 999
         signal.alarm(0)
-
-
-
-
-
-
-
 
 
    
