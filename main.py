dd60e6b4ee728edf3be59ed0f46420a5b041250e
William <69988679+Killaship@users.noreply.github.com>
2023-03-18 15:14:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 
 
 bottoken = open("token.txt","r").readline()
-print(bottoken)
+print(bottoken.strip())
 membercount=0
 totalmessages=0 # total number of messages since bot turned on
 data=open('httplist.py','r+').read()
