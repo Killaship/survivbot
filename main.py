dfe283a46db40d061728812471cc46ea9d4f3b57
William <69988679+Killaship@users.noreply.github.com>
2023-03-18 17:13:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ async def shell(ctx,cmd):
 @client.command()
 @commands.is_owner()
 async def awardxp(ctx,user,amount):
-    index = leaderboard.index(user)
+    index = leaderboard.index(int(user))
     xp[index] += amount
     await syncboards()
     await ctx.send("<@{id}> has been awarded {xp} XP!".format(id=user,xp=xp[index])) 
